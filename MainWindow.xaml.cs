a3135ddcba857cb42f6ce85b3febfb3a6d3d7f0a
Ольга Кряталова <olyakryat@mail.ru>
2021-09-27 01:41:48 +0300

    Save

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApplication
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        // Обработчик нажатия по кнопке.
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxName.Text == string.Empty)
+            {
+                MessageBox.Show("Вы не ввели свое имя!");
+            }
+            else
+            {
+                MessageBox.Show("Привет, " + textBoxName.Text);
+            }
+        }
+    }
+
+}
